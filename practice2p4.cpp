65bc2c5e076be6e4bee24a15823e6de1bf7c336d
ChrisYangtRi <xiaoke134127@gmail.com>
2016-05-19 21:25:50 +0800

    。
    。

---
+++ practice2p4.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main(){
+    unsigned u = 10, u2 = 42;
+    std::cout << " \x1234 " << std::endl;
+    std::cout << u - u2 << std::endl;
+}
---
+++ practice2p8.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    using namespace std;
+    cout << "2\115\n" << endl;
+    cout << "2 \t \115 \n"<< endl;
+}
